68fbfef18c8c4599b014a318ee551dac9fe448eb
omer maimon <omermaimon11111@gmail.com>
2026-01-21 19:31:31 +0200

    fix form

--- script.js
+++ script.js
@@ -17,12 +17,22 @@ document.querySelectorAll('.reveal').forEach((el) => observer.observe(el));
 // 3. טיפול בטופס
 document.getElementById('trainerForm').addEventListener('submit', function(e) {
     e.preventDefault();
+    
+    const myForm = e.target;
+    const formData = new FormData(myForm);
     const btn = document.getElementById('submit-btn');
+    
     btn.innerText = "שולח...";
     btn.disabled = true;
 
-    setTimeout(() => {
+    fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+    })
+    .then(() => {
         document.getElementById('trainerForm').style.display = 'none';
         document.getElementById('success-msg').style.display = 'block';
-    }, 1500);
+    })
+    .catch((error) => alert(error));
 });
